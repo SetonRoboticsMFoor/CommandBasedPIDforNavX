e93f61a88b1292655785ef1617ae7239a5417c5c
SetonRoboticsMFoor <48261500+SetonRoboticsMFoor@users.noreply.github.com>
2023-01-28 13:11:24 -0700

    Used the constants file, added defult behavior

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,9 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -11,24 +6,26 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.BalanceRobotCom;
 import frc.robot.subsystems.DriveTrainSub;
 
-
 public class RobotContainer {
 
   private final DriveTrainSub m_DriveTrainSub = new DriveTrainSub();
+  private final Joystick driveStick = new Joystick(Constants.JoystickChannel);
+  private final JoystickButton balanceButton2 = new JoystickButton(driveStick, Constants.JoystickBalanceToggleChannel);
 
-  private final Joystick driveStick = new Joystick(0);
-
-  
   public RobotContainer() {
+    m_DriveTrainSub.setDefaultCommand(new BalanceRobotCom(m_DriveTrainSub, driveStick.getRawAxis(0)));
     configureBindings();
   }
 
   private void configureBindings() {
-    new JoystickButton(driveStick, 1).whileTrue(new BalanceRobotCom(m_DriveTrainSub, 0));
+    new JoystickButton(driveStick, Constants.JoystickBalanceButtonChannel)
+        .whileTrue(new BalanceRobotCom(m_DriveTrainSub, 0));
+    
+        balanceButton2.toggleOnTrue(new BalanceRobotCom(m_DriveTrainSub, 0));
   }
+
 
   public Command getAutonomousCommand() {
     return null;
-
   }
 }
